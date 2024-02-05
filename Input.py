e5ac5a7ccff6fe9d3a547b7e233cc19c5616b623
Khajashaik1997 <Khajapeers263@gmail.com>
2024-02-05 09:41:44 +0530

    Commit Message

---
+++ Input.py
@@ -0,0 +1,12 @@
+
+
+weather = input('What is the forcast today ? (Rain,Snow,Sun)')
+
+if weather == "Rain" :
+    print("Remember Your Umbrella")
+elif  weather == "Snow":
+     print ("Wear warm clothes")
+elif weather == "Sun":
+     print("Go Out and Enjoy The Sunshine!")   
+else :
+     print("Please Mention the Correct Forecast") 
